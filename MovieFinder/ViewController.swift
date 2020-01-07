6ec4739a462876423ecccf4187756ae584bbea62
Alexis <al.orellano23@gmail.com>
2020-01-07 14:39:19 -0800

    Initial Commit

---
+++ MovieFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Alexis Orellano on 1/7/20.
+//  Copyright © 2020 Alexis Orellano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
