4d7442a83b988d06b0537bdc7db1893bfec9802c
Alexis <al.orellano23@gmail.com>
2020-01-09 22:11:08 -0800

    SignUp UI completed

--- MovieFinder/SignUpView.swift
+++ MovieFinder/SignUpView.swift
@@ -47,23 +47,20 @@ class SignUpView: UIView {
         backgroundColor = UIColor.backgroundColor
 
         placeSignUpHeader()
-        placeUsernameTextField()
-        placeEmailTextField()
-        placePasswordTextField()
+        setupUsernameTextField()
+        setupEmailTextField()
+        setupPasswordTextField()
         placeSignUpButton()
         placeLoginButton()
-        
     }
     
     func placeSignUpHeader() {
         addSubview(signUpHeader)
-        
         signUpHeader.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 35).isActive = true
-        
         signUpHeader.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     }
     
-    func placeUsernameTextField() {
+    func setupUsernameTextField() {
         addSubview(usernameTextField)
         
         usernameTextField.attributedPlaceholder = NSAttributedString(string: " Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryTextColor])
@@ -72,15 +69,16 @@ class SignUpView: UIView {
         usernameTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -50).isActive = true
     }
     
-    func placeEmailTextField() {
+    func setupEmailTextField() {
         addSubview(emailTextField)
+        
         emailTextField.attributedPlaceholder = NSAttributedString(string: " Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryTextColor])
         emailTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20).isActive = true
         emailTextField.leadingAnchor.constraint(equalTo: usernameTextField.leadingAnchor).isActive = true
         emailTextField.trailingAnchor.constraint(equalTo: usernameTextField.trailingAnchor).isActive = true
     }
     
-    func placePasswordTextField() {
+    func setupPasswordTextField() {
         addSubview(passwordTextField)
         passwordTextField.attributedPlaceholder = NSAttributedString(string: " Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryTextColor])
         passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20).isActive = true
@@ -88,15 +86,13 @@ class SignUpView: UIView {
         passwordTextField.trailingAnchor.constraint(equalTo: usernameTextField.trailingAnchor).isActive = true
     }
     
-
-    
     func placeSignUpButton() {
         addSubview(signUpButton)
         signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 50).isActive = true
         signUpButton.leadingAnchor.constraint(equalTo: passwordTextField.leadingAnchor).isActive = true
         signUpButton.trailingAnchor.constraint(equalTo: passwordTextField.trailingAnchor).isActive = true
-
-
+        
+        addSignUpButtonAction()
     }
     
     func placeLoginButton() {
@@ -106,7 +102,9 @@ class SignUpView: UIView {
         loginButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
     }
     
-    
+    func addSignUpButtonAction() {
+        signUpButton.addTarget(self, action: #selector(SignUpController.signUpButtonPressed), for: .touchUpInside)
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
